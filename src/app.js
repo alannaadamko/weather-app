82713a53407e4e1a1a91d11a0660088567e8f99d
alannaadamko <81330383+alannaadamko@users.noreply.github.com>
2021-04-19 20:05:43 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -65,7 +65,8 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
+  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
